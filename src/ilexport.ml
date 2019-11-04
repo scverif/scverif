13ce6c1328baa13efc7cb2a113c18a9f9f07f2c0
Marc <marc.gourjon@tuhh.de>
2019-11-04 15:36:08 +0100

    maskverif program serialization working again.

--- src/ilexport.ml
+++ src/ilexport.ml
@@ -22,7 +22,7 @@ let mv_pphdr_var fmt v =
       V.pp_g v
   | Tmem -> assert false
 
-let mv_pp_header fmt an =
+let mv_pp_header fmt (an, undefvar) =
   begin
     [@warning "-26"]
     let filter ty (t,v) ls =
@@ -35,11 +35,12 @@ let mv_pp_header fmt an =
     let sha_of l = List.fold_right (filter Ileval.Sharing) l [] in
     let rnd_of l = List.fold_right (filter Ileval.URandom) l [] in
     (* FIXME/TODO secrets need to be understood by maskverif or used for CT-type-checking *)
-    Format.fprintf fmt "@[<v>public inputs: %a@ input shares: %a@ output shares: %a@ randoms: %a;@]"
+    Format.fprintf fmt "@[<v>public inputs: %a@ input shares: %a@ output shares: %a@ randoms: %a@ others: %a;@]"
       (pp_list ", " mv_pphdr_var) (pub_of an.input_var)
       (pp_list ", " mv_pphdr_var) (sha_of an.input_var)
       (pp_list ", " mv_pphdr_var) (sha_of an.output_var)
       (pp_list ", " mv_pphdr_var) (rnd_of an.input_var)
+      (pp_list ", " mv_pphdr_var) undefvar
   end
 
 let mv_pp_body fmt tr =
@@ -55,9 +56,35 @@ let mv_pp_body fmt tr =
     (pp_list "@ " pp_b) tr
 
 let print_mv params st an m =
+  let undefvars =
+    let anvar = List.map snd (an.output_var @ an.input_var) in
+    let var_of_assgn_not_in_env vs (i:Il.instr) =
+      match i.i_desc with
+      | Il.Iassgn (Il.Lvar v, _)
+      | Il.Iassgn (Il.Lset (v, _), _) ->
+        if List.mem v vs || List.mem v anvar then
+          vs
+        else
+          [v]@vs
+      | Il.Iassgn (Il.Lstore (_, _, _), _) ->
+        Utils.hierror "Ilexport.print_mv" (Some (fst i.i_loc))
+          "@[Expecting evaluated program, cannot handle memory %a]"
+          (pp_i ~full:!Glob_option.full) i
+      | Il.Ileak (_, _)
+      | Il.Ilabel _ -> vs
+      | Il.Imacro (_, _)
+      | Il.Igoto _
+      | Il.Iigoto _
+      | Il.Iwhile (_, _, _)
+      | Il.Iif (_, _, _) ->
+        Utils.hierror "Ilexport.print_mv" (Some (fst i.i_loc))
+          "@[Expecting evaluated program, cannot handle %a]" (pp_i ~full:!Glob_option.full) i
+      in
+    List.fold_left var_of_assgn_not_in_env [] st.st_eprog
+  in
   Format.printf "@[<v>proc %s:@   %a@ @ %a@ end@ para noglitch %a %s@ @]@."
     m.mc_name
-    mv_pp_header an
+    mv_pp_header (an, undefvars)
     mv_pp_body st.st_eprog
     Scv.pp_scvcheckkind params
     m.mc_name
