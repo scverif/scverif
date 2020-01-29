8e7915302b6289dfed06392f5dea8eda7aab955a
Marc <marc.gourjon@tuhh.de>
2020-01-29 08:45:34 +0100

    nix with maskverif working

--- default.nix
+++ default.nix
@@ -1,11 +1,14 @@
-# Copyright 2019 - Inria, NXP
+# Copyright 2019-2020 - Inria, NXP
 
 with import <nixpkgs> {};
+
+let maskverif = import ../maskverif
+;in
 
 stdenv.mkDerivation {
   name = "scverif-0";
   src = ./.;
   buildInputs = [ ]
-    ++ (with ocamlPackages; [ ocaml findlib ocamlbuild menhir zarith merlin ocamlgraph batteries ppx_deriving ppx_import re])
+    ++ (with ocamlPackages; [ ocaml findlib ocamlbuild menhir zarith merlin ocamlgraph batteries ppx_deriving ppx_import re maskverif])
     ;
 }
